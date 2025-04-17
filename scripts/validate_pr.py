f668273ba1f2841362b8751ad057df8ddd0d37cf
Albert Sun <albertsun0@gmail.com>
2025-04-16 19:46:41 -0700

    Add error cases and leetcode validation

--- scripts/validate_pr.py
+++ scripts/validate_pr.py
@@ -3,6 +3,7 @@ import requests
 from utils.yaml_utils import parse_yaml, parse_yaml_file
 from utils.rest_utils import website_is_up
 from utils.write_action_output import set_multiline_output
+from utils.leetcode_utils import user_is_cracked_at_leetcode
 
 n = len(sys.argv)
 
@@ -26,7 +27,15 @@ def validate_pr(github_username):
         if site["username"] == github_username:
             user_count += 1
 
-    assert user_count == 1
+    if user_count != 1:
+        text = "Do you even exist?"
+        if user_count > 1:
+            text = "One site per user please."
+        set_multiline_output(
+            OUTPUT_VAR,
+            text,
+        )
+        sys.exit(-1)
 
     diff = []
 
@@ -39,7 +48,14 @@ def validate_pr(github_username):
             OUTPUT_VAR,
             "You have changed more than one site in your PR. Please only add one site per PR.",
         )
-        raise Exception()
+        sys.exit(-1)
+
+    if len(diff) == 0:
+        set_multiline_output(
+            OUTPUT_VAR,
+            "Did you even change anything...",
+        )
+        sys.exit(-1)
 
     change = diff[0]
 
@@ -52,13 +68,17 @@ def validate_pr(github_username):
     if change["username"] != github_username:
         set_multiline_output(
             OUTPUT_VAR,
-            "You have added a site to your PR, but it does not belong to you.",
+            "Don't touch other peoples sites please.",
         )
-        raise Exception()
+        sys.exit(-1)
 
     if not website_is_up(change["url"]):
         set_multiline_output(OUTPUT_VAR, "The site you have added is not live.")
-        raise Exception()
+        sys.exit(-1)
+
+    if not user_is_cracked_at_leetcode(change["lc_username"]):
+        set_multiline_output(OUTPUT_VAR, "Get Good at Leetcode.")
+        sys.exit(-1)
 
 
 # assert that user has only edited sites.yaml
@@ -70,7 +90,11 @@ if len(changed_files) > 1 and "sites.yaml" in changed_files:
     raise Exception()
 
 if len(changed_files) == 1 and changed_files[0] == "sites.yaml":
-    validate_pr(github_username)
+    try:
+        validate_pr(github_username)
+    except Exception as e:
+        set_multiline_output(OUTPUT_VAR, "Skill Diff")
+        sys.exit(-1)
 
 set_multiline_output(
     OUTPUT_VAR,
