65e4d76f504352c6f60b53e25a3b1965d7756438
Joshua Goon <25201575+Jish2@users.noreply.github.com>
2025-04-17 17:27:00 +0000

    update logs

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -12,6 +12,6 @@ export const getUsers = async () => {
 	const data = await fetch(url, { cache: "no-store" });
 	const text = await data.text();
 	const users = load(text) as User[];
-
+	console.log("users", users)
 	return users;
 };
